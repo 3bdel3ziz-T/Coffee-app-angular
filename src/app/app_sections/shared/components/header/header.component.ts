f97fb6468e8088fcce402f38032a153b74f96aac
3bdel3zizT <zizotamercomz@gmail.com>
2024-02-10 20:51:20 +0200

    fix some problems

--- src/app/app_sections/shared/components/header/header.component.ts
+++ src/app/app_sections/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgStyle, NgIf } from '@angular/common';
 
 @Component({
@@ -8,27 +8,12 @@ import { NgStyle, NgIf } from '@angular/common';
   standalone: true,
   imports: [NgStyle, NgIf]
 })
-export class HeaderComponent implements OnInit {
-
-  ngOnInit(): void {
-    const dateNow: Date = new Date,
-      day = dateNow.getDate(),
-      hours = dateNow.getHours(),
-      minutes = dateNow.getMinutes();
-    this.time = `${hours > 12 ? hours - 12 : hours } : ${minutes < 10 ? '0' : ''}${minutes} ${hours < 12 ? 'AM' : 'PM'}`
-
-    this.changeBackground();
-  }
+export class HeaderComponent {
 
   time!: string;
   signal: 25 | 50 | 75 | 100 = 100;
   batteryPercent: number = 60;
-  changeBG: boolean = false;
-
-  changeBackground(): void {
-    document.addEventListener('scroll', () => {
-      window.scrollY > 1 ? this.changeBG = true : this.changeBG = false;
-    });
+  constructor() {
+    this.time = (new Date).toLocaleTimeString(undefined, { timeStyle: 'short' })
   }
 }
-
